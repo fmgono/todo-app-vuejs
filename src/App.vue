f0464b5aa41f876ccbcd7123eae3bf46cba63db5
Fathan Margono <fmgonoo@gmail.com>
2020-02-09 12:51:58 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <create-todo v-on:create-todo="addTodo"></create-todo>
+    <todo-list v-bind:todos="todos"></todo-list>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList';
+import CreateTodo from './components/CreateTodo';
+
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+    CreateTodo,
+  },
+  // data function avails data to the template
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'Todo A',
+          project: 'Project A',
+          done: false,
+        },
+        {
+          id: 2,
+          title: 'Todo B',
+          project: 'Project B',
+          done: false,
+        },
+        {
+          id: 3,
+          title: 'Todo C',
+          project: 'Project C',
+          done: true,
+        },
+      ],
+    };
+  },
+  methods: {
+    addTodo(data) {
+      this.todos.push(data);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <p>Completed Tasks: {{todos.filter(todo => todo.done).length}}</p>
+    <p>Pending Tasks: {{todos.filter(todo => !todo.done).length}}</p>
+    <todo
+      v-for="todo in todos"
+      v-on:delete-todo="deleteTodo"
+      v-on:complete-todo="completeTodo"
+      v-bind:todo="todo"
+      v-bind:key="todo.id"
+    ></todo>
+  </div>
+</template>
+
+<script>
+import Todo from './Todo';
+
+export default {
+  props: ['todos'],
+  components: {
+    Todo,
+  },
+  methods: {
+    deleteTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos.splice(todoIndex, 1);
+    },
+    completeTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos[todoIndex].done = true;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
